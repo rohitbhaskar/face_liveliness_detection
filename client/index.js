b3d3c416e2d8f1406af9bd9699a894f08fe339fe
Rohit <rohitbhaskar1vs10@gmail.com>
2017-10-08 18:29:21 +0530

    restuctured files

--- client/index.js
+++ client/index.js
@@ -5,12 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('views'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-
-app.get('/', function(request, response) {
-  response.render('views/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
