14ad9ce7a31e8447a5031700dd87b62e67eb681b
Rohit <rohitbhaskar1vs10@gmail.com>
2017-10-08 18:00:21 +0530

    added start script

---
+++ client/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('views'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+
+app.get('/', function(request, response) {
+  response.render('views/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
